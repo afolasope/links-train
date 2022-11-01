ca6db9354b7358bb8c4f6b578a03850da96e2972
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-11-01 13:28:02 +0100

    fixes profile image styling

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,11 @@
-import profile from './profile__img.svg';
+import profile from './profile__img.jpg';
 import shareIcon from './share.svg';
 import shareMobile from './dots-horizontal.svg';
 import gitIcon from './Icon.svg';
 import slackIcon from './slack.svg';
 import I4GLogo from './I4G.svg';
 import ZuriLogo from './Zuri.Internship_Logo.svg';
+import cameraIcon from './camera.svg';
 
 export {
   profile,
@@ -14,4 +15,5 @@ export {
   slackIcon,
   I4GLogo,
   ZuriLogo,
+  cameraIcon
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import { profile, shareIcon, shareMobile } from '../assets';
+import React, { useState } from 'react';
+import { cameraIcon, profile, shareIcon, shareMobile } from '../assets';
 
 const Header = () => {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div className="header">
       <div className="img-container">
-        <img id='profile__img' src={profile} alt="user picture" />
-        <p id='twitter'>@_afolasope</p>
-        <p id='slack'>adebanjo afolasope</p>
+        <div className="profile-wrapper">
+          <div
+            className="img"
+            onClick={() => {
+              setClicked(!clicked);
+            }}
+          >
+            <img id="profile__img" src={profile} alt="user" />
+            <div className={clicked ? 'overlay' : null}></div>
+            <div className={clicked ? 'camera-icon' : 'hidden'}>
+              <img src={cameraIcon} alt="camera" />
+            </div>
+          </div>
+        </div>
+        <p id="twitter">@_afolasope</p>
+        <p id="slack">adebanjo afolasope</p>
       </div>
       <div className="share-icon">
-        <img src={shareIcon} alt="share" className='tablet-share' />
-        <img src={shareMobile} alt="share"  className='mobile-share'/>
+        <img
+          src={shareIcon}
+          alt="share"
+          className="tablet-share"
+        />
+        <img src={shareMobile} alt="share" className="mobile-share" />
       </div>
     </div>
   );
