17d2453551b765b742e6a3ae2a67b62f266d2e89
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-11-03 22:14:22 +0100

    chore: adds contacts form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <div>
+        <h1>Contact Me</h1>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      </div>
+      <form action="">
+        <div>
+          <label htmlFor="firstName"> First Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName"> Last Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="email"> Email</label>
+          <input type="email" />
+        </div>
+        <div>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div>
+          <input type="checkbox" name="" id="" />
+          <p>You agree to providing your data to  who may contact you.</p>
+        </div>
+        <div>
+          <button type="submit">Send Message</button>
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
