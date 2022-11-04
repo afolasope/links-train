1e1d8ec5c1684c800cd5d2501a39c021d0cb6ac6
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-11-04 10:00:34 +0100

    style: adds styles to contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,36 +2,44 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <>
-      <div>
+    <div className="contact-wrapper">
+      <div className="contact-header">
         <h1>Contact Me</h1>
         <p>Hi there, contact me to ask me about anything you have in mind.</p>
       </div>
       <form action="">
-        <div>
+        <div className="form-control">
           <label htmlFor="firstName"> First Name</label>
-          <input type="text" />
+          <input type="text" placeholder="Enter your first name" />
         </div>
-        <div>
+        <div className="form-control">
           <label htmlFor="lastName"> Last Name</label>
-          <input type="text" />
+          <input type="text" placeholder="Enter your last name" />
         </div>
-        <div>
-          <label htmlFor="email"> Email</label>
+        <div className="form-control">
+          <label htmlFor="email" placeholder="Enter your first name">
+            Email
+          </label>
           <input type="email" />
         </div>
-        <div>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
+        <div className="textarea">
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder={`Send me a message and I'll reply you as soon as possible...`}
+          ></textarea>
         </div>
-        <div>
+        <div className="agreement">
           <input type="checkbox" name="" id="" />
-          <p>You agree to providing your data to  who may contact you.</p>
+          <p>You agree to providing your data to who may contact you.</p>
         </div>
-        <div>
+        <div className="contact-submit">
           <button type="submit">Send Message</button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
